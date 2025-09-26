c49f45f429157bda8772debdbb8b1c7cd4074498
maryyammostafa <maryammostafaawad22@gmail.com>
2025-09-26 08:14:53 +0300

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -6,30 +6,10 @@ function getData(category = "business", search = ""){
             row = document.querySelector("section .row");
             row.innerHTML = ``;
         news.forEach((element) => {
-            if(search == ""){
+            if(search == "" || element.source.name?.toLowerCase().includes(search.toLowerCase()) || element.content?.toLowerCase().includes(search.toLowerCase()) || element.description?.toLowerCase().includes(search.toLowerCase()) || element.author?.toLowerCase().includes(search.toLowerCase())){
                 row.innerHTML += `
                     <div class="col-md-6 col-lg-4">
-                        <div class="item mb-5 p-4 rounded-5" style="background-image: url(${element.urlToImage || "..//Images//not-found.jpg"});">
-                            <a href="${element.url}" class="text-decoration-none text-black">
-                                <div class="box bg-light position-absolute bottom-0 start-0 rounded p-2 m-3">
-                                    <h5 class="text-primary">${element.source.name || "unknown"}</h5>
-                                    <p class="fs-5 fw-medium mb-2">${element.content?.substr(0,50) || "no content"}...</p>
-                                    <p class="mb-1 text-black-50">${element.description?.substr(0,50) || "no description"}...</p>
-                                    <div class="info d-flex align-items-center">
-                                        <i class="fa-solid fa-user me-3"></i>
-                                        <p class="mb-0 me-4">${element.author?.substr(0,10) || "unknown"}</p>
-                                        <p class="mb-0 text-black-50">${element.publishedAt?.substr(0,10) || "unknown"}</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                `;
-            }
-            else if(element.source.name?.toLowerCase().includes(search.toLowerCase()) || element.content?.toLowerCase().includes(search.toLowerCase()) || element.description?.toLowerCase().includes(search.toLowerCase()) || element.author?.toLowerCase().includes(search.toLowerCase())){
-                row.innerHTML += `
-                    <div class="col-md-6 col-lg-4">
-                        <div class="item mb-5 p-4 rounded-5" style="background-image: url(${element.urlToImage});">
+                        <div class="item mb-5 p-4 rounded-5" style="background-image: url(${element.urlToImage || "../Images/not-found.jpg"});">
                             <a href="${element.url}" class="text-decoration-none text-black">
                                 <div class="box bg-light position-absolute bottom-0 start-0 rounded p-2 m-3">
                                     <h5 class="text-primary">${element.source.name || "unknown"}</h5>
